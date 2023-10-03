c7af30799e856dc530856b0622e4000193111133
nko3_mac <nko3@njit.edu>
2023-10-02 20:11:32 -0400

    done with assigment 4 just need to figure out how to return the dates back to their normal state.

--- hw_2.sh
+++ hw_2.sh
@@ -1,36 +1,44 @@
-#!/bin/bash
+#!/opt/homebrew/bin/bash
 
-# filename=$1
+#get the file
+filename=$1
+#pattern to match on 
+city_pattern="[[:space:]][a-zA-Z]+[[:space:]][a-zA-Z]+[[:space:]]?[a-zA-Z]+"
+age_pattern="([0-9]+)-([0-9]+)-([0-9]+)"
+name_pattern="(^[a-zA-z]+\s[a-zA-Z]+)"
+#set the current year
+current_year=2023
+declare -A name_age_table
+declare -a result_array
+#the file passed is a valid file 
+if [ -f $filename ];then
+    while IFS= read -r line;do 
+        if [[ $line =~ $city_pattern ]];then
+            result_array+=("$line") 
+        fi
+    done < $filename
+    else
+        echo "not a valid file"
+fi
 
-# city_pattern=" ([a-z]+ [a-z]+( ,)?)( )?([a-z]+)?"
+for line in "${result_array[@]}";do
+    #echo "$line"
+    #apply the age_pattern to the line 
+    if [[ $line =~ $age_pattern ]];then
+        #extract the year from the user
+        year="${BASH_REMATCH[1]}"
+        #get the age of the user 
+        age=$(( $current_year - $year ))
+        name=$(echo "$line" | grep -E -o $name_pattern)
+        #store the name and age in the associate array 
+        name_age_table[$name]=$age
+    fi
 
-# year_extraction="(19[0-9]{1,2})|(20[0-9]{1,2})"
-# current_year=2023
-# declare -A name_age_table #store name along its age
-# result=$(grep -E  "$city_pattern" "$filename")
-# echo $result
-# # #after the name_age table has been populated pipe the result into sort in desending order 
-# # #print each name & age in the name_age table to the screen 
-# # #take the a screen shot of the result and submit 
-# # for key in "${!name_age_table[*]}";do
-# #     age_value="${name_age_table[$key]}"
-# #     echo "$key is $age_value"
-# # done | sort -rn -k3
+done
 
-
-
-
-
-# #  echo "$line"
-# #             if [[ $line =~ $year_extraction ]]; then
-# #                     #capture the first and last name at  the start of the line. store in name
-# #                     name="(^[a-z]+ [a-z]+)"
-# #                     calculated_age=$current_year - $year_extraction
-# #                     #append to the table
-# #                     name_age_table[$name]=$calculated_age
-                    
-# #                 else    
-# #                     echo "No date found on line"
-# #             fi
-# #         else    
-# #             echo "No match"
+#now that we have the name and age populated in the name_age_table we can print using the key 
+#I will be using a shortcut sort function to sort the item, effectively sort breaks the lines into column and since the age is on the 4th column 
+#i will use k4,4 (to specify the column) -r is to sort in a descending order and n or -n is to ensure it sorts in numeric fashion.
+for row in "${!name_age_table[@]}";do
+    echo "$row is ${name_age_table["$row"]}"
+done | sort -rn -k4,4
